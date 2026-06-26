de591c44af42d54b1920a11039c3d43bc2376cab
Gaetan Pawlowski <gaetanpawl@gmail.com>
2026-06-26 20:44:09 +0000

    feat: OSC52 clipboard support (tmux + nvim) and update LazyVim plugins

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.clipboard = "unnamedplus" -- yank -> presse-papier
+
+-- Force OSC 52 (indispensable en SSH, sans serveur graphique)
+local osc52 = require("vim.ui.clipboard.osc52")
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = { ["+"] = osc52.copy("+"), ["*"] = osc52.copy("*") },
+  paste = { ["+"] = osc52.paste("+"), ["*"] = osc52.paste("*") },
+}
